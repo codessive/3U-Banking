8ed781713b787387b4376c73d20de9e3eac2055e
Ephraimite <aniekanephraim@gmail.com>
2020-05-11 11:17:41 +0100

    worked on the UI

--- app/src/main/java/io/github/codessive/ubank/DashboardActivity.java
+++ app/src/main/java/io/github/codessive/ubank/DashboardActivity.java
@@ -1,10 +1,16 @@
 package io.github.codessive.ubank;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -12,6 +18,7 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
     }
 
     public void openFirstbank(View view) {
@@ -73,5 +80,6 @@ public class DashboardActivity extends AppCompatActivity {
         Intent intent = new Intent(this, ZenithBank.class);
         startActivity(intent);
     }
+
 
 }
--- app/src/main/java/io/github/codessive/ubank/GTB.java
+++ app/src/main/java/io/github/codessive/ubank/GTB.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckedTextView;
 import android.widget.TextView;
 
 import com.hover.sdk.actions.HoverAction;
@@ -14,8 +16,11 @@ import com.hover.sdk.api.HoverParameters;
 
 import java.util.ArrayList;
 
-public class GTB extends AppCompatActivity implements Hover.DownloadListener {
+public class GTB extends AppCompatActivity implements Hover.DownloadListener, View.OnClickListener {
     private final String TAG = "GTB";
+
+    private CheckedTextView buy_airtime,transfer_others,bill_payment,transfer_gtb,gtb_balance;
+    private Button proceed_payment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,19 +29,19 @@ public class GTB extends AppCompatActivity implements Hover.DownloadListener {
 
         Hover.initialize(getApplicationContext(), this);
 
+        gtb_balance =(CheckedTextView) findViewById(R.id.balance_gtb);
+        transfer_gtb =(CheckedTextView) findViewById(R.id.transfer_gtb);
+        bill_payment =(CheckedTextView) findViewById(R.id.bill_payment);
+        transfer_others =(CheckedTextView) findViewById(R.id.transfer_others_gtbbank);
+        buy_airtime =(CheckedTextView) findViewById(R.id.buy_airtime);
 
-        TextView checkBalance = findViewById(R.id.gtb_balance);
-        checkBalance.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v){
-        Intent i=new HoverParameters.Builder(GTB.this)
-        .request("58d4977a") // Add your action ID here
-//                    .extra("YOUR_VARIABLE_NAME", "TEST_VALUE") // Uncomment and add your variables if any
-        .buildIntent();
-        startActivityForResult(i,0);
-             }
-        });
+        proceed_payment = (Button) findViewById(R.id.btn_proceed_gtb);
+
+        proceed_payment.setOnClickListener(this);
+
     }
+
+
     @Override public void onError(String message) {
 //		Toast.makeText(this, "Error while attempting to download actions, see logcat for error", Toast.LENGTH_LONG).show();
         Log.e(TAG, "Error: " + message);
@@ -47,4 +52,14 @@ public class GTB extends AppCompatActivity implements Hover.DownloadListener {
         Log.d(TAG, "Successfully downloaded " + actions.size() + " actions");
     }
 
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.balance_gtb){
+            Intent i=new HoverParameters.Builder(GTB.this)
+                    .request("58d4977a") // Add your action ID here
+//                    .extra("YOUR_VARIABLE_NAME", "TEST_VALUE") // Uncomment and add your variables if any
+                    .buildIntent();
+            startActivityForResult(i,0);
+        }
+        }
 }
